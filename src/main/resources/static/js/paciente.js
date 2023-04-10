f19d63e6c7db42450d35fed1a836a782fa0b9ea3
Agustina Mastricola <agus.ppye@gmail.com>
2023-04-10 04:02:16 -0300

    Pacientes--Metodos POST, PUT, DELET - OK

--- src/main/resources/static/js/paciente.js
+++ src/main/resources/static/js/paciente.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
-    const submitSuccess = document.getElementById('submitSuccessMessage-pac');
-    const submitError = document.getElementById('submitErrorMessage-pac');
-    const url = 'http://localhost:8080/pacientes';
+    /* Formulario POST */
+    const containerFormPost = document.getElementById('contenedor-form-post-odon');
+    const formPost = document.getElementById('form-post-pac');
     const nombre = document.getElementById('nombre-pac');
     const apellido = document.getElementById('apellido-pac');
     const dni = document.getElementById("dni-pac");
@@ -9,21 +9,39 @@ window.addEventListener('load', function () {
     const numero = document.getElementById('numero');
     const localidad = document.getElementById('localidad');
     const provincia = document.getElementById("provincia");
+    const mensajeExitoso = document.getElementById('submitSuccessMessage-pac');
+    const mensajeError = document.getElementById('submitErrorMessage-pac');
 
-    const formulario = document.getElementById('paciente-form-post');
-    const containerForm = document.getElementById('contenedor-form-post_pac');
-    const containerFormUpd = document.getElementById('contenedor-form-put_pac');
-    const containerList = document.getElementById('contenedor-listado-pac');
-    const pacienteList = document.getElementById('lista-pacientes');
+    /* Formulario PUT */
+    const containerFormPut = document.getElementById('contenedor-form-put-pac');
+    const formPut = document.getElementById('form-put-pac');
+    const nombrePut = document.getElementById('nombre-pac-put')
+    const apellidoPut = document.getElementById('apellido-pac-put')
+    const dniPut = document.getElementById('dni-pac-put')
+    const callePut = document.getElementById('calle-put')
+    const numeroPut = document.getElementById('numero-put')
+    const localidadPut = document.getElementById('localidad-put')
+    const provinciaPut = document.getElementById('provincia-put')
+    const mensajeExitosoPut = document.getElementById('submitSuccessMessage-pac-put');
+    const mensajeErrorPut = document.getElementById('submitErrorMessage-pac-put');
+
+    /* Endpoints */
+    const url = 'http://localhost:8080/pacientes';
+    const urlPacAll = 'http://localhost:8080/pacientes/all';
+
+    /* Botones */
     const botonNuevo = document.getElementById('nuevo-pac');
     const botonListar = document.getElementById('listar-pac');
-    const urlAll = 'http://localhost:8080/pacientes/all';
+
+    /* Renderizado de Pacientes */
+    const containerListPac = document.getElementById('contenedor-listado-pac');
+    const listItemsPac = document.getElementById('listado-pacientes');
 
     /*---------- POST -------------*/
-    formulario.addEventListener('submit', function(event){
+    formPost.addEventListener('submit', function(event){
         event.preventDefault()
         postPaciente()
-        formulario.reset()
+        formPost.reset()
     })
 
     async function postPaciente() {
@@ -38,10 +56,10 @@ window.addEventListener('load', function () {
         try{
             const response = await fetch(url, settings);
             const datos = await response.json();
-            submitSuccess.classList.remove('d-none')
+            mensajeExitoso.classList.remove('d-none')
         }
         catch{
-            submitError.classList.remove('d-none')
+            mensajeError.classList.remove('d-none')
         }
     }
     function capturarDatos() {
@@ -54,7 +72,7 @@ window.addEventListener('load', function () {
         nombre: nombre.value,
         apellido: apellido.value,
         dni: dni.value,
-        fecha_ingreso: `${year}-${mes}-${dia}`,
+        fecha_ingreso: `${year}-${mes}-${dia}` ,
         domicilio: {
             calle: calle.value,
             numero: numero.value,
@@ -66,33 +84,31 @@ window.addEventListener('load', function () {
     }
     /* -------- BOTON CREAR 'NUEVO' -------------*/
     botonNuevo.addEventListener('click', function(event) {
-        containerList.classList.add('d-none');
-        containerForm.classList.remove('d-none');
+        containerListPac.classList.add('d-none');
+        containerFormPost.classList.remove('d-none');
     })
     /* ----------- BOTON 'VER TODOS' ---------------*/
     botonListar.addEventListener('click', function(event){
-        containerForm.classList.add('d-none');
-        containerFormUpd.classList.add('d-none');
+        containerFormPost.classList.add('d-none');
+        containerFormPut.classList.add('d-none');
         listarTodos();
-        containerList.classList.remove('d-none');
+        containerListPac.classList.remove('d-none');
     })
 
     /*----------- LISTAR TODOS ---------------------*/
     async function listarTodos(){
-            const response = await fetch(urlAll);
+            const response = await fetch(urlPacAll);
             const datos = await response.json();
             renderizarPaciente(datos)
     }
     function renderizarPaciente(list){
-        pacienteList.innerHTML = '';
+        listItemsPac.innerHTML = '';
         list.forEach( paciente => {
-
-            pacienteList.innerHTML += `
-                <tr id="fila-paciente-${paciente.id}">
+            listItemsPac.innerHTML += `
+                <tr id="item-pac-${paciente.id}">
                     <td class="invisible">${paciente.id}</td>
                     <td>${paciente.nombre} ${paciente.apellido}</td>
                     <td>${paciente.dni}</td>
-                    <td>${paciente.fecha_ingreso}</td>
                     <td>${paciente.domicilio.calle} ${paciente.domicilio.numero} ${paciente.domicilio.localidad}</td>
                     <td>
                         <button class="btn btn-primary m-2 boton-modificar-pac" id="${paciente.id}">Modificar</button>
@@ -115,12 +131,12 @@ window.addEventListener('load', function () {
         })
     }
     function eliminarById(id){
-        const idFila = document.getElementById('fila-paciente-' + id);
+        const idItem = document.getElementById('item-pac-' + id);
         const settings = {
             method: 'DELETE'
         }
         fetch(`${url}/${id}`, settings)
-        .then(idFila.remove())
+        .then(idItem.remove())
     }
 
     /*------------ MODIFICAR DATOS - PUT -----------------*/
@@ -128,37 +144,28 @@ window.addEventListener('load', function () {
         const botonModificar = document.querySelectorAll('.boton-modificar-pac');
         botonModificar.forEach(boton => {
             boton.addEventListener('click', (event) => {
-                containerList.classList.add('d-none');
-                containerFormUpd.classList.remove('d-none');
+                containerListPac.classList.add('d-none');
+                containerFormPut.classList.remove('d-none');
                 const id = event.target.id
                 putPaciente(id)
+
             })
         })
     }
 
     function putPaciente(id){
-        const formUpd = document.getElementById('paciente-form-put_pac');
-        const nombreUpd = document.getElementById('nombre-upd-pac')
-        const apellidoUpd = document.getElementById('apellido-upd-pac')
-        const dniUpd = document.getElementById('dni-upd-pac')
-        const calleUpd = document.getElementById('calle-upd')
-        const numeroUpd = document.getElementById('numero-upd')
-        const localidadUpd = document.getElementById('localidad-upd')
-        const provinciaUpd = document.getElementById('provincia-upd')
-        const updateSuccessUpd = document.getElementById('submitSuccessMessage-upd-pac')
-
-        formUpd.addEventListener('submit', function(event){
+        formPut.addEventListener('submit', function(event){
             event.preventDefault()
             const formData = {
                 id: id,
-                nombre: nombreUpd.value,
-                apellido: apellidoUpd.value,
-                dni: dniUpd.value,
+                nombre: nombrePut.value,
+                apellido: apellidoPut.value,
+                dni: dniPut.value,
                 domicilio: {
-                    calle: calleUpd.value,
-                    numero: numeroUpd.value,
-                    localidad: localidadUpd.value,
-                    provincia: provinciaUpd.value
+                    calle: callePut.value,
+                    numero: numeroPut.value,
+                    localidad: localidadPut.value,
+                    provincia: provinciaPut.value
                 }
             }
             const settings = {
@@ -169,8 +176,7 @@ window.addEventListener('load', function () {
                 body: JSON.stringify(formData)
             }
             fetch(url, settings)
-            .then(res => res.json())
-            .then(updateSuccessUpd.classList.remove('d-none'))
+            .then(mensajeExitosoPut.classList.remove('d-none'))
         })
     }
 })
